cb34f3949cac31efe1f4e6b37fcea38b072a9df6
Camilla Jácome <camilla.cstrc@gmail.com>
2022-11-28 19:16:10 -0300

    resolve testes do FavoritPokemon

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o componente favoritePokemon', () => {
+  test('se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha Pokémon favoritos;', () => {
+    renderWithRouter(<App />);
+    const favoriteLink = screen.getByRole('link', { name: /favorite/i });
+    userEvent.click(favoriteLink);
+    const notFound = screen.getByText(/no favorite pokémon found/i);
+    expect(notFound).toBeInTheDocument();
+  });
+  test('se são exibidos todos os cards de Pokémon favoritados', () => {
+    renderWithRouter(<App />);
+    const detailsLink = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(detailsLink);
+    const checkboxFavorite = screen.getByRole('checkbox', { name: /pokémon favoritado\?/i });
+    userEvent.click(checkboxFavorite);
+    const favoriteLink = screen.getByRole('link', { name: /favorite pokémon/i });
+    userEvent.click(favoriteLink);
+    const pikachu = screen.getByText(/pikachu/i);
+    expect(pikachu).toBeInTheDocument();
+  });
+});
