809dc5fd7c26e5b184675bc2cfa0afa6f9335895
Camilla Jácome <camilla.cstrc@gmail.com>
2022-11-28 19:36:23 -0300

    resolve testes do NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import { screen, act } from '@testing-library/react';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('testa o componente NotFound', () => {
+  test('se a página contém um heading h2 com o texto Page requested not found', () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => {
+      history.push('/unkown');
+    });
+    const notFound = screen.getByRole('heading', {
+      name: /page requested not found/i,
+      level: 2,
+    });
+    expect(notFound).toBeInTheDocument();
+  });
+
+  test('se a página mostra a imagem "https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif"', () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => {
+      history.push('/unknown');
+    });
+    const img = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const image = screen.getByRole('img'); expect(image.src).toBe(img);
+  });
+});
